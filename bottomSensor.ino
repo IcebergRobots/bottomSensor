91924d06b3eb10748caf1279e2e5d5bf7c1974b6
Unknown <fribolin.stecker@gmail.com>
2019-01-26 12:34:28 +0100

    calibrate() implemented

--- bottomSensor.ino
+++ bottomSensor.ino
@@ -10,15 +10,20 @@
 
 #define PLEXEN D12
 
-byte binPins[] = {D0, D1, D2, D3};  //die 4 Pins fuer die Multiplexer
-int value[]  = value{};             //ausgelesene Werte
-bool nus[] = nus[];                 //not used sensors
+byte binPins[] = {D0, D1, D2, D3};      //die 4 Pins fuer die Multiplexer
+int value[48];                          //ausgelesene Werte
+bool nus[48];                           //not used sensors
+bool hit[48];                           //ausgeschlagene Sensoren
+int threshold[48];                      //Schwellwerte fuer jeden Sensor
+long stamp = 0;
+int maxValue[48];
+int minValue[48];
 
 void setup(){
-    for(int i = 2; i <= 13; i++){   //digitale Pins auf Input
+    for(int i = 2; i <= 13; i++){       //digitale Pins auf Input
         pinMode(i, INPUT);
     }
-    pinMode(A0, INPUT);             //analoge Pins auf Input
+    pinMode(A0, INPUT);                 //analoge Pins auf Input
     pinMode(A4, INPUT);
     pinMode(A5, INPUT);
     pinMode(A6, INPUT);
@@ -45,7 +50,13 @@ void loop(){
 void measure(){
     int state;
 
-    for(int counter = 0; counter <= 16; counter++){
+    for(int counter = 0; counter < 16; counter++){
+
+        hit[counter] = false;
+        hit[counter + 16] = false;
+        hit[counter + 32] = false;
+
+
         String bin = String(counter, BIN);
         int binlength = bin.length();
 
@@ -53,19 +64,104 @@ void measure(){
             bin = "0" + bin;
         }
 
-    for(int i = 0; i <= 4; i++){
-        if(bin[i] == 0) state = LOW;
-        if(bin[i] == 1) state = HIGH;
-        digitalWrite(binPins[i], state);
+        for(int i = 0; i <= 4; i++){
+            if(bin[i] == 0) state = LOW;
+            if(bin[i] == 1) state = HIGH;
+            digitalWrite(binPins[i], state);
+        }
+
+        value[counter] = analogRead(S1);
+        value[counter + 16] = analogRead(S2);
+        value[counter + 32] = analogRead(S3);
+
+        if(value[counter] <= threshold[counter]){
+            hit[counter] = true;
+            interrupt();
+        } 
+        if(value[counter + 16] <= threshold[counter + 16]){
+            hit[counter + 16] = true;
+            interrupt();
+        } 
+        if(value[counter + 32] <= threshold[counter + 32] && counter <= 8){
+            hit[counter + 16] = true;
+            interrupt();
+        } 
+
+    
+    }
+}
+
+calibrate(){
+    stamp = millis() + 10000;
+    for(int counter = 0; counter < 16; counter++){
+            
+        String bin = String(counter, BIN);
+        int binlength = bin.length();
+
+        for(int i = 0; i <= 4-binlength; i++){
+        bin = "0" + bin;
+        }
+
+        for(int i = 0; i <= 4; i++){
+            if(bin[i] == 0) state = LOW;
+            if(bin[i] == 1) state = HIGH;
+            digitalWrite(binPins[i], state);
+        }
+
+        value[counter] = analogRead(S1);
+        value[counter + 16] = analogRead(S2);
+        value[counter + 32] = analogRead(S3);
+
+        maxValue[counter] = value[counter];
+        minValue[counter] = value[counter];
+
+        maxValue[counter + 16] = value[counter + 16];
+        minValue[counter + 16] = value[counter + 16];
+
+        maxValue[counter + 32] = value[counter + 32];
+        maxValue[counter + 32] = value[counter + 32];
     }
 
-    value[counter] = analogRead(S1);
-    value[counter + 16] = analogRead(S2);
-    value[counter + 32] = analogRead(S3);
 
+    while(millis() < stamp){
+        for(int counter = 0; counter < 16; counter++){
+            
+            String bin = String(counter, BIN);
+            int binlength = bin.length();
+
+            for(int i = 0; i <= 4-binlength; i++){
+            bin = "0" + bin;
+            }
+
+            for(int i = 0; i <= 4; i++){
+                if(bin[i] == 0) state = LOW;
+                if(bin[i] == 1) state = HIGH;
+                digitalWrite(binPins[i], state);
+            }
+
+            value[counter] = analogRead(S1);
+            value[counter + 16] = analogRead(S2);
+            value[counter + 32] = analogRead(S3);
+
+            if(value[counter] > maxValue[counter])maxValue[counter] = value[counter];
+            if(value[counter] < minValue[counter])minValue[counter] = value[counter];
+
+            if(value[counter + 16] > maxValue[counter + 16])maxValue[counter + 16] = value[counter + 16];
+            if(value[counter + 16] < minValue[counter + 16])minValue[counter + 16] = value[counter + 16];
+
+            if(value[counter + 32] > maxValue[counter + 32])maxValue[counter + 32] = value[counter + 32];
+            if(value[counter + 32] > maxValue[counter + 32])maxValue[counter + 32] = value[counter + 32];
+        }
+    }
+
+
+    for(int counter = 0; counter < 16; counter ++){
+        threshold[counter] = (maxValue[counter] + minValue[counter])/2;
+        threshold[counter + 16] = (maxValue[counter + 16] + minValue[counter + 16])/2;
+        threshold[counter + 32] = (maxValue[counter + 32] + minValue[counter + 32])/2;
     }
 }
 
 void specialPrint(){
-  
+    Serial.println("                " + 100);
 }
